b48e69e94747bbb5ad0a9b387a5af27c35384cdd
zlatimirCS <zlatimir_rk@yahoo.com>
2023-03-31 19:28:49 +0200

    layout started

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -35,9 +35,21 @@ const ProtectedRoute = ({ children }) => {
   console.log('user', user);
 
   return (
-    <div>
-      {user?.name}
-      {children}
+    <div className='h-screen w-screen bg-gray-100 p-2'>
+      {/*header*/}
+      <div className='flex justify-between p-5'>
+        <div className='flex items-center gap-1'>
+          <i className='ri-wechat-fill text-2xl'></i>
+          <h1 className='text-primary text-2xl uppercase font-bold'>Chat</h1>
+        </div>
+        <div className='flex items-center gap-1'>
+          <i class='ri-shield-user-line text-xl'></i>
+          <h1 className='underline text-xl'>{user?.name}</h1>
+        </div>
+      </div>
+
+      {/*content - pages*/}
+      <div className='p-5'>{children}</div>
     </div>
   );
 };
