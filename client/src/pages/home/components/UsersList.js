d237bf517bc9904304ead81edd9d3e42a1763e1d
zlatimirCS <zlatimir_rk@yahoo.com>
2023-04-03 17:24:06 +0200

    all chats

--- client/src/pages/home/components/UsersList.js
+++ client/src/pages/home/components/UsersList.js
@@ -2,40 +2,39 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const UsersList = ({ searchKey }) => {
-  let { allUsers } = useSelector((state) => state.userReducer);
-  let { allChats } = useSelector((state) => state.userReducer);
+  let { allUsers, allChats } = useSelector((state) => state.userReducer);
   console.log('all Chats', allChats);
 
   return (
     <div className='flex flex-col gap-3 mt-5'>
       {allUsers
         .filter(
-          (user) =>
-            user.name.toLowerCase().includes(searchKey.toLowerCase()) &&
+          (userObj) =>
+            userObj.name.toLowerCase().includes(searchKey.toLowerCase()) &&
             searchKey
         )
-        .map((user) => {
+        .map((userObj) => {
           return (
             <div
-              key={user._id}
+              key={userObj._id}
               className='shadow-sm border p-5 rounded-2xl bg-white'
             >
               <div className='flex gap-2 items-center'>
-                {user.profilePic ? (
+                {userObj.profilePic ? (
                   <img
-                    src={user.profilePic}
+                    src={userObj.profilePic}
                     alt='profile pic'
                     className='w-10 h-10 rounded-full'
                   />
                 ) : null}
-                {!user.profilePic && (
+                {!userObj.profilePic && (
                   <div className='bg-gray-500 rounded-full h-10 w-10 flex items-center justify-center'>
                     <h1 className='uppercase text-2xl font-bold text-white'>
-                      {user.name[0]}
+                      {userObj.name[0]}
                     </h1>
                   </div>
                 )}
-                <h1>{user.name}</h1>
+                <h1>{userObj.name}</h1>
               </div>
             </div>
           );
