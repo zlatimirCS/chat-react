d7f859e5906bd55d2ccb6354840cc7e5c4af8dfc
zlatimirCS <zlatimir_rk@yahoo.com>
2023-04-04 20:21:57 +0200

    display chat messages

--- client/src/pages/home/components/ChatArea.js
+++ client/src/pages/home/components/ChatArea.js
@@ -59,34 +59,46 @@ const ChatArea = () => {
 
   console.log('messages', messages);
   return (
-    <div className='bg-white h-[85vh] border rounded-2xl w-full flex flex-col justify-between p-5'>
-      {/*1st part receipent user*/}
-      <div>
-        <div className='flex gap-2 items-center mb-2'>
-          {receipentUser.profilePic ? (
-            <img
-              src={receipentUser.profilePic}
-              alt='profile pic'
-              className='w-10 h-10 rounded-full'
-            />
-          ) : null}
-          {!receipentUser.profilePic && (
-            <div className='bg-gray-500 rounded-full h-10 w-10 flex items-center justify-center'>
-              <h1 className='uppercase text-2xl font-bold text-white'>
-                {receipentUser.name[0]}
-              </h1>
-            </div>
-          )}
-          <h1 className='uppercase'>{receipentUser.name}</h1>
+    <div className='bg-white h-[85vh] border rounded-2xl w-full flex flex-col p-5'>
+      <div style={{ flex: 1 }}>
+        {/*1st part receipent user*/}
+        <div>
+          <div className='flex gap-2 items-center mb-2'>
+            {receipentUser.profilePic ? (
+              <img
+                src={receipentUser.profilePic}
+                alt='profile pic'
+                className='w-10 h-10 rounded-full'
+              />
+            ) : null}
+            {!receipentUser.profilePic && (
+              <div className='bg-gray-500 rounded-full h-10 w-10 flex items-center justify-center'>
+                <h1 className='uppercase text-2xl font-bold text-white'>
+                  {receipentUser.name[0]}
+                </h1>
+              </div>
+            )}
+            <h1 className='uppercase'>{receipentUser.name}</h1>
+          </div>
+          <hr />
         </div>
-        <hr />
-      </div>
 
-      {/*2nd part chat messages*/}
-      <div>
-        {messages.map((message) => {
-          return <p key={message._id}>{message.text}</p>;
-        })}
+        {/*2nd part chat messages*/}
+        <div className='mt-5'>
+          <div className='flex flex-col gap-2'>
+            {messages.map((message) => {
+              const isCurrentUserSender = message.sender === user._id;
+              return (
+                <div className={`flex ${isCurrentUserSender && 'justify-end'}`}>
+                  <div className='flex flex-col'>
+                    <h1>{message.text}</h1>
+                    <h1>{message.createdAt}</h1>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
 
       {/*3rd part chat input*/}
